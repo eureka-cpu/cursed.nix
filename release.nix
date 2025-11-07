6d0381b54433418808d3b25950f3d4b420d06e38
eureka-cpu <github.eureka@gmail.com>
2025-11-07 05:56:36 -0800

    5. Add static and aarch64 for default target

--- release.nix
+++ release.nix
@@ -1,6 +1,8 @@
 let
   sources = import ./npins;
   pkgs = import sources.nixpkgs { };
+  pkgsStatic = pkgs.pkgsStatic;
+  aarch64Pkgs = pkgs.pkgsCross.aarch64-multiplatform;
 
   gosend = pkgs.callPackage ./gosrc/send.nix { };
   goserve = pkgs.callPackage ./gosrc/serve.nix { inherit pysend; };
@@ -8,6 +10,10 @@ let
   luaserve = pkgs.callPackage ./luasrc/serve.nix { inherit gosend; };
   pysend = pkgs.callPackage ./pysrc/send.nix { };
   pyserve = pkgs.callPackage ./pysrc/serve.nix { inherit luasend; };
+  curse = pkgs.callPackage ./rustsrc { };
+
+  curse-static = pkgsStatic.callPackage ./rustsrc { };
+  curse-aarch64 = aarch64Pkgs.callPackage ./rustsrc { };
 in
 {
   inherit
@@ -17,6 +23,10 @@ in
     luaserve
     pysend
     pyserve
+    curse
     ;
-  curse = pkgs.callPackage ./rustsrc { };
+  inherit
+    curse-static
+    curse-aarch64
+    ;
 }
